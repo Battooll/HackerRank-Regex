82e8f2b54e7015d17ffa47792460b3389e8d95d9
Batool Hammoud <batoolhammoud1995@gmail.com>
2022-04-23 17:47:38 +0300

    Update Matching Same Text Again & Again.py

--- Backreferences/Matching Same Text Again & Again.py
+++ Backreferences/Matching Same Text Again & Again.py
@@ -5,7 +5,8 @@ Created on Thu Apr 21 17:19:35 2022
 @author: FPCC
 """
 
-regex_pattern = r"hackerrank"	# Do not delete 'r'.
+Regex_Pattern = r'^([a-z])(\w)(\s)(\W)(\d)(\D)([A-Z])([a-zA-Z])([aeiouAEOIU])(\S)\1\2\3\4\5\6\7\8\9\10$'	# Do not delete 'r'.
+
 
 import re
 import sys
